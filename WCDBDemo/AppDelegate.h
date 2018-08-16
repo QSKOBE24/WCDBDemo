885773004b73c3f48379678276990a15ca8127d2
398706336@qq.com <398706336@qq.com>
2018-08-16 15:46:03 +0800

    proj init

---
+++ WCDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCDBDemo
+//
+//  Created by sukeqiu on 2018/8/16.
+//  Copyright © 2018年 sukeqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
